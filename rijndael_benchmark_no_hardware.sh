5096f19c1c49eeab5666f5b5b5b672411d3203b7
Jason Brake <jasbrake@gmail.com>
2020-04-30 20:16:51 -0400

    Fix typo

--- rijndael_benchmark_no_hardware.sh
+++ rijndael_benchmark_no_hardware.sh
@@ -2,4 +2,4 @@
 
 mkdir -p out
 javac -d "out" -cp "lib/*" src/RijndaelBenchmark.java
-java -cp "lib/*:out" -XX:-UseAES -XX:-UseAESInstrinsics RijndaelBenchmark "rijndael_no_hardware_results.csv"
+java -cp "lib/*:out" -XX:-UseAES -XX:-UseAESIntrinsics RijndaelBenchmark "rijndael_no_hardware_results.csv"
