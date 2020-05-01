ac247504a14f0393a5c7f3bb91178ca69dad35df
Jason Brake <jasbrake@gmail.com>
2020-04-30 20:39:13 -0400

    Fix java compilation for rijndael

--- benchmarks/rijndael.sh
+++ benchmarks/rijndael.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p out
-javac -d "out" -cp "lib/*" src/RijndaelBenchmark.java
+javac -d "out" -cp "lib/*" src/RijndaelBenchmark.java src/Rijndael/*.java
 java -cp "lib/*:out" RijndaelBenchmark "rijndael_results.csv"
--- benchmarks/rijndael_no_hardware.sh
+++ benchmarks/rijndael_no_hardware.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p out
-javac -d "out" -cp "lib/*" src/RijndaelBenchmark.java
+javac -d "out" -cp "lib/*" src/RijndaelBenchmark.java src/Rijndael/*.java
 java -cp "lib/*:out" -XX:-UseAES -XX:-UseAESIntrinsics RijndaelBenchmark "rijndael_no_hardware_results.csv"
