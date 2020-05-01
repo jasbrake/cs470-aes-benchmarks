2bf94355a5fde5a01465cabd1b16b5c42fb32eba
Jason Brake <jasbrake@gmail.com>
2020-04-30 20:20:40 -0400

    Only create data files if they don't exist

--- generate_data.sh
+++ generate_data.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-head -c 256MB < /dev/urandom > data256.bin
-head -c 512MB < /dev/urandom > data512.bin
-head -c 1024MB < /dev/urandom > data1024.bin
+if [ ! -f data256.bin ]; then
+	head -c 256MB < /dev/urandom > data256.bin
+fi
+if [ ! -f data512.bin ]; then
+	head -c 512MB < /dev/urandom > data512.bin
+fi
+if [ ! -f data1024.bin ]; then
+	head -c 1024MB < /dev/urandom > data1024.bin
+fi
